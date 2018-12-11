e8a902b1209a6f37be6cc21eb267f4505159f70f
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2018-12-11 13:41:45 +0100

    add login_required decorator to topics view

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -10,6 +11,7 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     topics = Topic.objects.order_by('date_added')
     ctx = {
